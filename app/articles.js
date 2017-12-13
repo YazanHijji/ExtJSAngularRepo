3fa05841dfaa297c7dbaf997e36b97311909fc7e
Yazan Hijji <yhijji23@gmail.com>
2017-12-14 01:57:52 +0200

    file not needed anymore

--- app/articles.js
+++
@@ -1,7 +0,0 @@
-angular.module('articles', [])
-.controller('ArticlesController', function($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
-        then(function(response) {
-            $scope.returnedArticles = response.data;
-        });
-});
